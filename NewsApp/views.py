84a44c5cb42deff1d943724dc05554bc76bac554
Rassul Aitkali <workdir@MacBook-Air-Rassul.local>
2024-04-05 22:03:09 +0500

    Setup Django environment and add web-scrapping parser in NewsApp/views.py

---
+++ NewsApp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from bs4 import BeautifulSoup
+import requests
+# Create your views here.
+def index(request):
+    original_link = 'https://tengrinews.kz'
+    html_text = requests.get('https://tengrinews.kz/news/page/1/').text
+    soup = BeautifulSoup(html_text, 'lxml')
+    news = soup.find_all('div', class_='content_main_item')
+
+    title = []
+    link = []
+    image_link = []
+    for new in news:
+        new_link = original_link + new.a['href']
+        link.append(new_link)
+        new_link = original_link + new.a.picture.source['srcset']
+        image_link.append(new_link)
+        title.append(new.find('span', class_='content_main_item_title').a.text)
+    news = zip(link, image_link, title)
+    return render(request, 'newsapp/index.html', {'news': news})
