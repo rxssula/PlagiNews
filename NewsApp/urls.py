8d047fcbcd1b194d1d881cf4e963d4a4df0eefa8
rxssula <rxssul.aitkali@gmail.com>
2024-04-07 21:46:17 +0500

    Debug index.html file

--- NewsApp/urls.py
+++ NewsApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index, name="index"),
+    path('', views.index, name="index"),
     path('search/', views.search, name="search"),
     path('sport/', views.sport, name="sport"),
     path('edu/', views.edu, name="edu"),
