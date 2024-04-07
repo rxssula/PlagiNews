f43928f4eeb4690ad69529909cb9dd8033b7453d
rxssula <rxssul.aitkali@gmail.com>
2024-04-07 18:36:19 +0500

    Add edu.html and sport.html

--- NewsApp/urls.py
+++ NewsApp/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('index/', views.index, name="index"),
     path('search/', views.search, name="search"),
+    path('sport/', views.sport, name="sport"),
+    path('edu/', views.edu, name="edu"),
 ]
--- NewsApp/views.py
+++ NewsApp/views.py
@@ -3,6 +3,41 @@ from bs4 import BeautifulSoup
 import requests
 # Create your views here.
 
+def edu(request):
+    original_link = 'https://tengrinews.kz'
+    html_text = requests.get('https://tengrinews.kz/newseducation/').text
+    soup = BeautifulSoup(html_text, 'lxml')
+    news = soup.find_all('div', class_='content_main_item')
+
+    title = []
+    link = []
+    image_link = []
+    for new in news:
+        new_link = original_link + new.a['href']
+        link.append(new_link)
+        new_link = original_link + new.a.picture.source['srcset']
+        image_link.append(new_link)
+        title.append(new.find('span', class_='content_main_item_title').a.text)
+    news = zip(link, image_link, title)
+    return render(request, 'NewsApp/edu.html', {'news': news})
+
+def sport(request):
+    original_link = 'https://tengrisport.kz'
+    html_text = requests.get('https://tengrisport.kz/tnsport/').text
+    soup = BeautifulSoup(html_text, 'lxml')
+    news = soup.find_all('div', class_='content_main_item')
+
+    title = []
+    link = []
+    image_link = []
+    for new in news:
+        new_link = original_link + new.a['href']
+        link.append(new_link)
+        new_link = original_link + new.a.picture.source['srcset']
+        image_link.append(new_link)
+        title.append(new.find('span', class_='content_main_item_title').a.text)
+    news = zip(link, image_link, title)
+    return render(request, 'NewsApp/sport.html', {'news': news})
 def search(request):
     if request.method == "POST":
         searched = request.POST['searched']
